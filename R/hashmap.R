e77cc009b522ea0fe21f80a2c0b6b441431b8ad1
dirmeier <simon.dirmeier@bsse.ethz.ch>
2017-06-25 20:00:56 +0200

    Added constructor dor hashmap

--- R/hashmap.R
+++ R/hashmap.R
@@ -28,53 +28,25 @@
 #'  of key-value pairs. Inserting and accessing is amortized in \emph{O(1)}.
 #'  \code{hashmap} wraps a C++ \code{unordered_map} using Rcpp modules.
 #'
-#' @slot .map an Rcpp module wrapping an \code{unordered_map}
+#' @slot .list  object that bundles all important map related objects
 setClass(
     "hashmap",
-     slots     = list(.map      = "ANY"),
-     prototype = prototype(.map = NULL)
+     slots = list(.data = "list"),
+     prototype = prototype(.data = NULL)
 )
 
-#' @title Add key-value pairs to a \code{hashmap}-object
-#'
-#' @export
-#' @docType methods
-#' @rdname insert-methods
-#'
-#' @param obj  the hashmap you want to insert pairs to
-#' @param keys  a scalar/vector of keys
-#' @param values  a scalar/vector of values
-#' @examples
-#'   hashmap <- new("hashmap")
-#'   insert(hashmap, "test", 1)
-setGeneric(
-    "insert",
-    function(obj, keys, values)
-    {
-        standardGeneric("insert")
-    },
-    package="datastructures"
-)
-
-#' @rdname insert-methods
-#' @aliases insert,character,character-method
+#' @noRd
 setMethod(
-    "insert",
-    signature = signature(obj="hashmap", keys="character", values="character"),
-    function(obj, keys, values)
+    "initialize",
+    "hashmap",
+    function(.Object,
+             keys   = c("character", "numeric", "integer"),
+             values = c("character", "numeric", "integer"))
     {
-        if (is.null(obj@.map))
-        {
-            cat("flower")
-            obj@.map <- methods::new(hashmap_ss)
-        }
-        else if (class(obj@.map) != "Rcpp_hashmap_ss")
-        {
-            stop(paste0("cannot insert <character,character> to ", class(obj@.map)[1]))
-        }
-        cat("sinserting")
-        obj@.map$insert(keys, values)
-        return(obj)
+        .Object@.data <- list(key.class   = match.arg(keys),
+                              value.class = match.arg(keys))
+        # init
+
+        .Object
     }
 )
-
